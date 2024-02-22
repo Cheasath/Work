629ed8ed1a8b3790cad18b41a1b9f6fc1fdb4431
Cheasath <89178918+Cheasath@users.noreply.github.com>
2024-02-22 08:21:27 +0530

    Update Sumofnum_dma.c

--- Sumofnum_dma.c
+++ Sumofnum_dma.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n,*arr,sum = 0;    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    arr = (int *)malloc(n * sizeof(int));
+    
+    // Check if memory allocation was successful
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed!");
+        return 1; // Exit with error
+    }
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    printf("Sum of elements = %d\n", sum);
+    free(arr);
+    return 0;
+}
 
